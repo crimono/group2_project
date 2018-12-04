aa8f482aa9c34c3493ac6302dc7c76186b29cce3
louwiii <louis.polet@unil.ch>
2018-12-04 19:30:05 +0100

    Update loop retrieve tweets.R

--- loop retrieve tweets.R
+++ loop retrieve tweets.R
@@ -3,7 +3,7 @@ lookup_coords("california")
 # Give values to all the states
 devtools::install_github("mkearney/rtweet")
 library(rtweet)
-twitter_data_grouped <- matrix(NA, ncol = 88, nrow = 5000)
+twitter_data_grouped <- matrix(NA, ncol = 88, nrow = 50000)
 states <- c("Alabama", "Ohio", "California", "Texas", "Alaska")
 states_coord <- matrix(NA, ncol = 2, nrow = 50)
 for (i in 1:5) {
@@ -25,16 +25,7 @@ for (i in 1:5) {
 
 #------------------------------------------------------------------------------
 
-twitter_data_grouped <- list()
-states <- c("Alabama", "Ohio", "California", "Texas", "Alaska")
-for (i in 1:5) {
-  twitter_data_grouped <- search_tweets(
-    q = "#climatechange", 
-    n = 1000, 
-    geocode = lookup_coords(address = states[i]),
-    lang = "en",
-    include_rts = FALSE)
-}
+
 
 #------------------------------------------------------------------------------
 pot <- matrix(NA, ncol = 1, nrow = 10)
