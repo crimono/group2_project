37ff1c60e8914f0e4cc23b178713ab5159723cc3
louwiii <louis.polet@unil.ch>
2018-12-05 18:40:44 +0100

    Create Rcpp function to limit value favorite_count

---
+++ TwitterMoodUSA/src/fav_limit.cpp
@@ -0,0 +1,31 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+NumericVector fav_limit(NumericVector twitter_data_filtered) {
+
+  NumericVector data(twitter_data_filtered.length();
+
+  double mean_fav = mean(twitter_data_filtered);
+  double sd_fav = 3*sd(twitter_data_filtered);
+  double limit =  mean_fav + sd_fav;
+
+  for(int i(1); i<=twitter_data_filtered.length(); i++)
+  {
+    if (limit < 5)
+    {
+      data(i - 1) = twitter_data_filtered(i) + 1; 
+    }
+    else if (twitter_data_filtered(i)) >= limit)
+    {
+      twitter_data_filtered(i)) = limit;
+      data(i - 1) = twitter_data_filtered(i) + 1;
+    }
+    else
+    {
+      data(i - 1) = twitter_data_filtered(i) + 1;
+    }
+  }
+
+  return data;
+}
